6e8c51f0f6ff57f42aea829558977a1d23d5f13e
Sergiu Breban <breban.sergiu94@gmail.com>
2018-01-08 21:33:32 +0200

    Remove comments

--- compare.py
+++ compare.py
@@ -1,22 +1,8 @@
-# USAGE
-# python compare.py --dataset images
-
-import argparse
 import glob
 
 import cv2
-# import the necessary packages
 import matplotlib.pyplot as plt
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-                help="Path to the directory of images")
-args = vars(ap.parse_args())
-
-# initialize the index dictionary to store the image name
-# and corresponding histograms and the images dictionary
-# to store the images themselves
 index = {}
 images = {}
 div = 64
@@ -31,70 +17,48 @@ def reduceColor():
                 image[x, y, z] = image[x, y, z] / div * div + div / 2
 
 
-# loop over the image paths
-for imagePath in glob.glob(args["dataset"] + "/*.png"):
-    # extract the image filename (assumed to be unique) and
-    # load the image, updating the images dictionary
+for imagePath in glob.glob("images/*.png"):
     filename = imagePath[imagePath.rfind("/") + 1:]
     image = cv2.imread(imagePath)
-    reduceColor()
+    # reduceColor()
     images[filename] = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # extract a 3D RGB color histogram from the image,
-    # using 8 bins per channel, normalize, and update
-    # the index
     hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8],
                         [0, div, 0, div, 0, div])
     hist = cv2.normalize(hist, hist).flatten()
     index[filename] = hist
 
-# METHOD #1: UTILIZING OPENCV
-# initialize OpenCV methods for histogram comparison
 OPENCV_METHODS = (
     ("Correlation", cv2.HISTCMP_CORREL),
     ("Chi-Squared", cv2.HISTCMP_CHISQR),
     ("Intersection", cv2.HISTCMP_INTERSECT),
     ("Hellinger", cv2.HISTCMP_BHATTACHARYYA))
 
-# loop over the comparison methods
 for (methodName, method) in OPENCV_METHODS:
-    # initialize the results dictionary and the sort
-    # direction
     results = {}
     reverse = False
 
-    # if we are using the correlation or intersection
-    # method, then sort the results in reverse order
     if methodName in ("Correlation", "Intersection"):
         reverse = True
 
-    # loop over the index
     for (k, hist) in index.items():
-        # compute the distance between the two histograms
-        # using the method and update the results dictionary
         d = cv2.compareHist(index["doge.png"], hist, method)
         results[k] = d
 
-    # sort the results
     results = sorted([(v, k) for (k, v) in results.items()], reverse=reverse)
 
-    # show the query image
     fig = plt.figure("Query")
     ax = fig.add_subplot(1, 1, 1)
     ax.imshow(images["doge.png"])
     plt.axis("off")
 
-    # initialize the results figure
     fig = plt.figure("Results: %s" % (methodName))
     fig.suptitle(methodName, fontsize=20)
 
-    # loop over the results
     for (i, (v, k)) in enumerate(results):
-        # show the result
         ax = fig.add_subplot(1, len(images), i + 1)
         ax.set_title("%.2f" % v)
         plt.imshow(images[k])
         plt.axis("off")
 
-# show the OpenCV methods
 plt.show()
